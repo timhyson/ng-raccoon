f051a86331b9097a118fba7a36313323aa395062
Tim Hyson <tim.hyson@wonga.com>
2018-07-20 16:15:59 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AppNavbarComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    NgbModule.forRoot()
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
